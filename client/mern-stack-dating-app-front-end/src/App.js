186b0240118dc4fa6d40d169e849e1792bacc305
mrralphpierre <mrralphpierre@gmail.com>
2022-04-07 20:20:12 -0400

    added react front end

---
+++ client/mern-stack-dating-app-front-end/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import Header from './components/header/Header';
+import DatingCards from './components/datingCards/DatingCards';
+import SwipeButtons from './components/swipeButtons/SwipeButtons';
+
+
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+      <DatingCards />
+      <SwipeButtons />
+
+
+    </div>
+  );
+}
+
+export default App;
